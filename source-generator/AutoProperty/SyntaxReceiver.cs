3ed9e2f14f1e1c50e9f84eaa826b9e10bec4ac02
yenmoc <phongsoyenmoc.diep@gmail.com>
2023-12-21 11:32:46 +0700

    -add: auto property generator

---
+++ source-generator/AutoProperty/SyntaxReceiver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace source_generator;
+
+public class SyntaxReceiver : ISyntaxReceiver
+{
+    public List<(FieldDeclarationSyntax field, AttributeSyntax syntax)> TargetFields { get; } = [];
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is FieldDeclarationSyntax {AttributeLists.Count: > 0} field)
+        {
+            foreach (var list in field.AttributeLists)
+            {
+                foreach (var attribute in list.Attributes)
+                {
+                    var nameAttriute = attribute.Name.ToString();
+                    if (nameAttriute.EndsWith("AutoPropertyAttribute") || nameAttriute.EndsWith("AutoProperty"))
+                    {
+                        TargetFields.Add((field, attribute));
+                    }
+                }
+            }
+        }
+    }
+}
